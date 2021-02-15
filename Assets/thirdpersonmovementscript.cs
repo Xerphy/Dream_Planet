a1bde03eae15213f92a83c02463a1f6bfede600d
Dustin Schimel <dustin.schimel@gmail.com>
2021-02-15 00:56:27 -0700

    Added jumping and gravity
    Bug: When jumping against a wall while holding forward it causes the character to spaz out.

--- Assets/thirdpersonmovementscript.cs
+++ Assets/thirdpersonmovementscript.cs
@@ -7,16 +7,32 @@ public class thirdpersonmovementscript : MonoBehaviour
     public CharacterController controller;
     public Transform camera;
 
-    public float speed = 6f;
+    public float speed = 6f;    // Speed of character movement
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVel;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);  // Checks if the invisible sphere below the player collides with the ground
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");  // Captures W, S, Up Arrow, and Down Arrow key inputs
+        float vertical = Input.GetAxisRaw("Vertical");  // Captures A, D, Left Arrow, and Right Arrow key inputs
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if(direction.magnitude >= 0.1f)
@@ -28,5 +44,13 @@ public class thirdpersonmovementscript : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);  // Moves the character based on gravity
     }
 }
