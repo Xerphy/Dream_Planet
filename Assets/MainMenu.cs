92f8a1ced81c822da58bdeab151c817197430bd2
hatucolo77 <78497615+hatucolo77@users.noreply.github.com>
2021-03-16 17:12:24 -0600

    Full Start Menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -8,4 +8,8 @@ public class MainMenu : MonoBehaviour
     public void Playgame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
 }
