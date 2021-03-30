96e7cd56f6535a67504e41d2828f3327533db33c
RyanBahmanyar <u1128975@umail.utah.edu>
2021-03-29 19:17:05 -0600

    shooting laser beams
    Laser prefabs can now be shot from the firepoint
    The shots are sideways, which needs to be fixed

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,7 +5,7 @@ public class Gun : MonoBehaviour
     public float range = 100f;
 
     [SerializeField]
-    private Transform firePoint;//Point where laser beams will originate from
+    //private Transform firePoint;//Point where laser beams will originate from
 
     public GameObject laserPrefab;
     public float fireRate;
@@ -28,13 +28,13 @@ public class Gun : MonoBehaviour
 
     void Shoot()
     {
-        if(Physics.Raycast(firePoint.position, firePoint.forward, out hit, range))
+        if(Physics.Raycast(transform.position, transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
-            Debug.DrawRay(firePoint.position, firePoint.forward * 100, Color.red, 3f);
+            Debug.DrawRay(transform.position, transform.forward * 100, Color.red, 3f);
 
             GameObject laser = GameObject.Instantiate(laserPrefab, transform.position, transform.rotation) as GameObject;
-            //laser.GetComponent<ShotBehavior>().setTarget(hit.point);
+            laser.GetComponent<ShotBehavior>().setTarget(hit.point);
             GameObject.Destroy(laser, 2f);
         }
     }
