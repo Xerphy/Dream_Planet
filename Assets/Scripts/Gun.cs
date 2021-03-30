63942da0b02aeea378f322e76c98199ecfd51a23
RyanBahmanyar <u1128975@umail.utah.edu>
2021-03-29 18:58:10 -0600

    Refined gun script and added shotbehavior script

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,24 +5,37 @@ public class Gun : MonoBehaviour
     public float range = 100f;
 
     [SerializeField]
-    private Transform firePoint;
+    private Transform firePoint;//Point where laser beams will originate from
+
+    public GameObject laserPrefab;
+    public float fireRate;
+    private float shootRateTimeStamp;
+
+    RaycastHit hit;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetButtonDown("Fire1"))
         {
-            Shoot();
+            if(Time.time > shootRateTimeStamp)
+            {
+                Shoot();
+                shootRateTimeStamp = Time.time + fireRate;
+            }
         }
     }
 
     void Shoot()
     {
-        RaycastHit hit;
         if(Physics.Raycast(firePoint.position, firePoint.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
             Debug.DrawRay(firePoint.position, firePoint.forward * 100, Color.red, 3f);
+
+            GameObject laser = GameObject.Instantiate(laserPrefab, transform.position, transform.rotation) as GameObject;
+            //laser.GetComponent<ShotBehavior>().setTarget(hit.point);
+            GameObject.Destroy(laser, 2f);
         }
     }
 }
