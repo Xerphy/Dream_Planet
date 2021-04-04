fcc04016c20ff23faf5a85ece5c71335b31fbf99
Dustin Schimel <dustin.schimel@gmail.com>
2021-04-03 21:34:22 -0600

    Removed astroboy model for slice and put back in default

--- Assets/Scripts/PlayerAdditions.cs
+++ Assets/Scripts/PlayerAdditions.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class PlayerAdditions : MonoBehaviour
 {
-    //checkpoint respawn coord
-    public Vector3 respawnLocation;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -17,18 +14,6 @@ public class PlayerAdditions : MonoBehaviour
     void Update()
     {
 
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Checkpoint")  //if player reaches a checkpoint, then respawnLocation is set to the position of said checkpoint
-        {
-            respawnLocation = other.transform.position;
-        }
-        else if (other.tag == "DeathZone")
-        {
-            transform.position = respawnLocation;   //teleport player to last checkpoint when they fall off level
-        }
     }
 }
 
