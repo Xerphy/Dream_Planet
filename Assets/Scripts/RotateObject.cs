a0c8b19deeebe9524d79db46d38183199bc94bae
Dustin Schimel <dustin.schimel@gmail.com>
2021-04-07 00:51:18 -0600

    Added rotating powerups

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class RotateObject : MonoBehaviour
 {
+    // User Inputs
     public float speed;
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    void Start()
+    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        // Rotate object around Y-Axis
         transform.Rotate(0, speed, 0);
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
     }
 }
