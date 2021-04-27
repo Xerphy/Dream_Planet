210a563e1a5409b295a865dcec42bb1778d4c6e2
Dustin Schimel <dustin.schimel@gmail.com>
2021-04-26 22:17:46 -0600

    Small changes to health

--- Assets/Scripts/HealthandDamage.cs
+++ Assets/Scripts/HealthandDamage.cs
@@ -7,23 +7,42 @@ using UnityEngine.UI;
 
 public class HealthandDamage : MonoBehaviour
 {
+    //checkpoint respawn coord
+    public Vector3 respawnLocation;
+
     public Text UIHealth;
 
     public int playerHealth = 5;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "EnemyBullet" || other.tag == "DeathZone")
+        if (other.tag == "Checkpoint")  //if player reaches a checkpoint, then respawnLocation is set to the position of said checkpoint
         {
-            playerHealth -= 1;
-
+            respawnLocation = other.transform.position;
+        }
+        else if (other.tag == "EnemyBullet")
+        {
             if (playerHealth <= 0)
             {
-                SceneManager.LoadScene(3);  //upon dying, the player will be sent back to the hub
+                SceneManager.LoadScene(2);  //upon dying, the player will be sent back to the hub
                 playerHealth = 5;   //health is reset
             }
 
             UIHealth.text = "Health: " + playerHealth;
+            playerHealth -= 1;
+        }
+        else if (other.tag == "DeathZone")
+        {
+            transform.position = respawnLocation;//teleport player to last checkpoint when they fall off level
+
+            if (playerHealth <= 0)
+            {
+                SceneManager.LoadScene(2);  //upon dying, the player will be sent back to the hub
+                playerHealth = 5;   //health is reset
+            }
+
+            UIHealth.text = "Health: " + playerHealth;
+            playerHealth -= 1;
         }
     }
 }
